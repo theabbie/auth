92f0a74b668a05c6faa87c2b0a14ee54a18b21c7
abhishek <abhishek7gg7@gmail.com>
2019-09-22 16:51:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,46 @@ var axios = require("axios");
 app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 600000}}))
 
 app.get("/otp", function(req, res) {
-req.session.otp = Math.floor(1000*Math.random()).toString();
-axios("https://srvrr.tk/mail?to="+req.query.mail+"&sub=otp&body="+req.session.otp).then(function(x) {
-res.end();
-})
+if(req.session.otp) {
+res.type("text/html").end(`
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://cdn.jsdelivr.net/gh/theabbie/theabbie.github.io/files/circle-cropped.png">
+
+    <title>OTP</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.0/examples/sign-in/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin" method="GET" action="/verify">
+      <img class="mb-4" src="https://cdn.jsdelivr.net/gh/theabbie/theabbie.github.io/files/circle-cropped.png" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Please enter OTP for ${req.session.mail}</h1>
+      <input name="otp" type="text" id="inputEmail" class="form-control" placeholder="OTP" required autofocus>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+    </form>
+  </body>
+</html>
+`);
+}
+else {
+res.redirect(301, "https://awth.now.sh/register");
+}
 })
 
-app.get('/*', function(req, res) {
-  if (req.session.otp) {
-    
-  } else {
-    res.type("text/html").end(`
+app.get('/register', function(req, res) {
+res.type("text/html").end(`
 <!doctype html>
 <html lang="en">
   <head>
@@ -36,11 +65,11 @@ app.get('/*', function(req, res) {
   </head>
 
   <body class="text-center">
-    <form class="form-signin" method="GET" action="/otp">
+    <form class="form-signin" method="GET" action="/api">
       <img class="mb-4" src="https://cdn.jsdelivr.net/gh/theabbie/theabbie.github.io/files/circle-cropped.png" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please Register</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <input name="mail" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
@@ -48,7 +77,23 @@ app.get('/*', function(req, res) {
   </body>
 </html>
 `);
-  }
+})
+
+app.get("/api", function(req, res) {
+req.session.mail = req.query.mail;
+req.session.otp = Math.floor(1000*Math.random()).toString();
+axios("https://srvrr.tk/mail?to="+req.session.mail+"&sub=otp&body="+req.session.otp).then(function(x) {
+res.redirect(301, "https://awth.now.sh/otp");
+})
+})
+
+app.get("/verify", function(req, res) {
+if (req.query.otp==req.session.otp) {res.end("Succesfully verified "+req.session.mail);req.session.destroy();}
+else {res.redirect(301, "https://awth.now.sh/otp")}
+})
+
+app.get("/*", function(req, res) {
+res.redirect(301, "https://awth.now.sh/register");
 })
 
 app.listen(process.env.PORT);
