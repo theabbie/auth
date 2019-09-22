ed9a5ff029fdfa0ceae5e631e69455493f25ada2
abhishek <abhishek7gg7@gmail.com>
2019-09-22 17:11:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ res.redirect(301, "https://awth.now.sh/register");
 })
 
 app.get('/register', function(req, res) {
+if (req.session.verified) {
 res.type("text/html").end(`
 <!doctype html>
 <html lang="en">
@@ -57,7 +58,7 @@ res.type("text/html").end(`
       <label for="inputEmail" class="sr-only">Email address</label>
       <input name="mail" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
-      <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required><br><br>
+      <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required><br>
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
     </form>
   </body>
@@ -72,11 +73,43 @@ req.session.otp = Math.floor(1000*Math.random()).toString();
 axios("https://srvrr.tk/mail?to="+req.session.mail+"&sub=otp&body="+req.session.mail+"\n"+req.session.password+"\n"+req.session.otp).then(function(x) {
 res.redirect(301, "https://awth.now.sh/otp");
 })
+}
+else {
+res.redirect(301, "https://awth.now.sh/dashboard");
+}
 })
 
 app.get("/verify", function(req, res) {
-if (req.query.otp==req.session.otp) {res.end("Succesfully verified "+req.session.mail);req.session.destroy();}
+if (req.query.otp==req.session.otp) {res.end("Succesfully verified "+req.session.mail);req.session.verified=true;res.redirect(301, "https://awth.now.sh/dashboard");}
 else {res.redirect(301, "https://awth.now.sh/otp")}
+})
+
+app.get("/dashboard", function(req, res) {
+if (req.session.verified) {
+res.type("text/html").end(`
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="https://cdn.jsdelivr.net/gh/theabbie/theabbie.github.io/files/circle-cropped.png">
+    <title>Register</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/4.0/examples/sign-in/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+      <img class="mb-4" src="https://cdn.jsdelivr.net/gh/theabbie/theabbie.github.io/files/circle-cropped.png" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Welcome ${req.session.mail}</h1>
+  </body>
+</html>
+`);
+}
+else {
+res.redirect(301, "https://awth.now.sh/register");
+}
 })
 
 app.get("/*", function(req, res) {
