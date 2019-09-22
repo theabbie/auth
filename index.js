c7226f004aaa27e294ba24105134115380148505
abhishek <abhishek7gg7@gmail.com>
2019-09-22 13:12:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var app = require("express")();
+var session = require("express-session");
+app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+ 
+// Access the session as req.session
+app.get('/', function(req, res, next) {
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write(`<meta http-equiv="refresh" content="1">`)
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
+  }
+})
 
+app.listen(process.env.PORT);
