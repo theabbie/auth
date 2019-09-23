b1a410cb772fa5045684207fab8fdf45f158f99c
abhishek <abhishek7gg7@gmail.com>
2019-09-23 10:20:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ res.type("text/html").end(`
   <form method="GET" action="/data">
     <div class="form-group">
       <label for="comment"><a href="/logout">Save Data:</a></label>
+      <label for="comment">Save Data:</label>
       <textarea name="data" class="form-control" rows="5" id="comment" ondblclick="this.form.submit()"></textarea>
       <p><big><br>${req.session.data || ""}</big></p>
     </div>
